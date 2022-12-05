feabf7e51802fa1986bd20485085779151bdcb70
tealblu <ckhartsell@gmail.com>
2022-12-05 17:53:21 -0500

    day 2 done!

--- rockPaperScissors.cpp
+++ rockPaperScissors.cpp
@@ -17,71 +17,158 @@
 
 using namespace std;
 
+class PointConverter {
+    public:
+        PointConverter() {
+            points['A'] = 1;
+            points['B'] = 2;
+            points['C'] = 3;
+
+            points['X'] = 1;
+            points['Y'] = 2;
+            points['Z'] = 3;
+
+            points['W'] = 6;
+            points['L'] = 0;
+            points['D'] = 3;
+        }
+        int getPoints(char c) {
+            return points[c];
+        }
+        int getPoints(pair<char, char> p) {
+            char me = p.second;
+            char them = p.first;
+
+            // Catch draw
+            if ((me == 'X' && them == 'A') || (me == 'Y' && them == 'B') || (me == 'Z' && them == 'C')) {
+                return getPoints('D') + getPoints(me);
+            }
+
+            // Figure out who won
+            if (me == 'X' && them == 'B') { // X (Rock) loses to B (Paper)
+                return getPoints('L') + getPoints(me);
+
+            } else if (me == 'X' && them == 'C') { // X (Rock) beats C (Scissors)
+                return getPoints('W') + getPoints(me);
+
+            } else if (me == 'Y' && them == 'A') { // Y (Paper) beats to A (Rock)
+                return getPoints('W') + getPoints(me);
+
+            } else if (me == 'Y' && them == 'C') { // Y (Paper) loses to C (Scissors)
+                return getPoints('L') + getPoints(me);
+
+            } else if (me == 'Z' && them == 'A') { // Z (Scissors) loses to A (Rock)
+                return getPoints('L') + getPoints(me);
+
+            } else if (me == 'Z' && them == 'B') { // Z (Scissors) beats B (Paper)
+                return getPoints('W') + getPoints(me);
+
+            } else {
+                cout << "Error: " << me << " vs " << them << endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        pair<char, char> decode(pair<char, char> next) {
+            // Decode the next move
+            char me;
+            char them = next.first;
+            char result = next.second;
+
+            if (result == 'Z') { // I won
+                if (them == 'A') { // They played Rock
+                    me = 'Y'; // I played Paper
+                } else if (them == 'B') { // They played Paper
+                    me = 'Z'; // I played Scissors
+                } else if (them == 'C') { // They played Scissors
+                    me = 'X'; // I played Rock
+                }
+            } else if (result == 'X') { // I lost
+                if (them == 'A') { // They played Rock
+                    me = 'Z';
+                } else if (them == 'B') { // They played Paper
+                    me = 'X';
+                } else if (them == 'C') { // They played Scissors
+                    me = 'Y';
+                }
+            } else if (result == 'Y') { // Draw
+                if (them == 'A') { // They played Rock
+                    me = 'X';
+                } else if (them == 'B') { // They played Paper
+                    me = 'Y';
+                } else if (them == 'C') { // They played Scissors
+                    me = 'Z';
+                }
+            } else {
+                cout << "Error: " << them << " vs " << result << endl;
+                exit(EXIT_FAILURE);
+            }
+
+            return(make_pair(them, me));
+        }
+        
+    private:
+        map<char, int> points;
+};
+
+class StrategyGuide {
+    public:
+        StrategyGuide() {
+            // Open file
+            ifstream file("strategyGuide.txt");
+
+            // Read file
+            string line;
+            while (getline(file, line)) {
+                strat.push_back(make_pair(line[0], line[2]));
+            }
+
+            // Close file
+            file.close();
+
+            current = 0;
+        }
+
+        pair<char, char> getNext() {
+            pair<char, char> next = strat[current];
+            current++;
+            return next;
+        }
+
+        bool hasNext() {
+            return current < strat.size();
+        }
+
+        void restart() {
+            current = 0;
+        }
+    private:
+        vector<pair<char, char>> strat;
+        int current;
+};
+
 int main() {
-    vector<pair<char, char>> strategyGuide;
+    // Initalize variables
+    PointConverter pc;
+    StrategyGuide sg;\
     int total = 0;
-    map<char, int> scores;
 
-    // Insert score values
-    scores.insert(pair<char, int>('R', 1));
-    scores.insert(pair<char, int>('P', 2));
-    scores.insert(pair<char, int>('S', 3));
-    scores.insert(pair<char,int>('L', 0));
-    scores.insert(pair<char, int>('T', 3));
-    scores.insert(pair<char, int>('W', 6));
+    // Loop through strategy guide
+    while (sg.hasNext()) {
+        // Get next move
+        pair<char, char> next = sg.getNext();
 
-    // Open file
-    ifstream strategyFile("strategyGuide.txt");
+        // Decode move
+        next = pc.decode(next);
+        cout << "I played " << next.second << " and they played " << next.first << endl;
 
-    // Check if file is open
-    if (strategyFile.is_open()) {
-        // Read file
-        string line;
-        while (getline(strategyFile, line)) {
-            // Add to vector
-            strategyGuide.push_back(make_pair(line[0], line[2]));
-            cout << "Added " << line[0] << " " << line[2] << endl;
-        }
+        // Get points
+        int p = pc.getPoints(make_pair(next.first, next.second));
+        cout << "I got " << p << " points" << endl;
+
+        // Add round points to total
+        total += p;
     }
 
-    // Close file
-    strategyFile.close();
-
-    // Iterate through vector, totaling scores
-    // Rock: A, X; Paper: B, Y; Scissors: C, Z
-    // Points: Rock: 1, Paper: 2, Scissors: 3, Lose: 0, Tie: 3, Win: 6
-    for (int i = 0; i < strategyGuide.size(); i++) {
-        int roundScore = 0;
-        if (strategyGuide[i].first == 'A' && strategyGuide[i].second == 'X') { // Rock vs Rock (tie)
-            roundScore += scores.at('R') + scores.at('T');
-
-        } else if (strategyGuide[i].first == 'B' && strategyGuide[i].second == 'Y') { // Paper vs Paper (tie)
-            roundScore += scores.at('P') + scores.at('T');
-
-        } else if (strategyGuide[i].first == 'C' && strategyGuide[i].second == 'Z') { // Scissors vs Scissors (tie)
-            roundScore += scores.at('S') + scores.at('T');
-
-        } else if (strategyGuide[i].first == 'X' && strategyGuide[i].second == 'B') { // Rock vs Paper (lose)
-            roundScore += scores.at('R') + scores.at('L');
-
-        } else if (strategyGuide[i].first == 'Y' && strategyGuide[i].second == 'C') { // Paper vs Scissors (lose)
-            roundScore += scores.at('P') + scores.at('L');
-
-        } else if (strategyGuide[i].first == 'Z' && strategyGuide[i].second == 'A') { // Scissors vs Rock (lose)
-            roundScore += scores.at('S') + scores.at('L');
-
-        } else if(strategyGuide[i].first == 'X' && strategyGuide[i].second == 'C') { // Rock vs Scissors (win)
-            roundScore += scores.at('R') + scores.at('W');
-            
-        } else if (strategyGuide[i].first == 'Y' && strategyGuide[i].second == 'A') { // Paper vs Rock (win)
-            roundScore += scores.at('P') + scores.at('W');
-            
-        } else if (strategyGuide[i].first == 'Z' && strategyGuide[i].second == 'B') { // Scissors vs Paper (win)
-            roundScore += scores.at('S') + scores.at('W');
-        }
-
-        total += roundScore;
-    }
-
-    cout << "total score: " << total << endl;
+    cout << "Total points: " << total << endl;
 }
